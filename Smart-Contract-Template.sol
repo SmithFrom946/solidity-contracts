8597a90e33b48544773685ead9dcbedaacca471d
Cal Smith <129142756+SmithFrom946@users.noreply.github.com>
2023-03-28 13:53:49 -0700

    update Smart-Contract-Template.sol

--- Smart-Contract-Template.sol
+++ Smart-Contract-Template.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// @devs this is a simple contract layout for properly setting up your first contract
-// this is free to use by anyone to layout their own projects in solidity
-// please use at your own risk, this is only template not a complete contract, do any ad all research before copyin and pasting// someone elses code. Never deploy anything you dont fully understand 
-// this was created by C.Smith aka cryptonic_chronic, march 2023
-
 // code intro/statement to devs
+  // @devs this is a simple contract layout for properly setting up your first contract
+  // this is free to use by anyone to layout their own projects in solidity
+  // please use at your own risk, this is only template not a complete contract, do any ad all research before copyin and pasting// someone elses code. Never deploy anything you dont fully understand 
+  // this was created by C.Smith aka cryptonic_chronic, march 2023
 
 // contract imports 
 
