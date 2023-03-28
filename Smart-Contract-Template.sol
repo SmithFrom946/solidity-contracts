dabc6a3ab07432f6d34f975a290fe0e4eb7b1a56
Cal Smith <129142756+SmithFrom946@users.noreply.github.com>
2023-03-28 13:51:11 -0700

    update Smart-Contract-Template.sol

---
+++ Smart-Contract-Template.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// @devs this is a simple contract layout for properly setting up your first contract
+// this is free to use by anyone to layout their own projects in solidity
+// please use at your own risk, this is only template not a complete contract, do any ad all research before copyin and pasting// someone elses code. Never deploy anything you dont fully understand 
+// this was created by C.Smith march 2023
+
+// code intro/statement to devs
+
+// contract imports 
+
+// contract
+
+// global variables
+ 
+// global mappings
+
+// structs && events
+
+// constuctor function
+
+// modifier functions
+
+// setter functions(changes state)
+
+// getter functions(viewable/pure)
